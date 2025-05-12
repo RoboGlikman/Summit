5050defa238081aa28c02a102d30c8d8183facf8
RoboGlikman <itamarglikman@gmail.com>
2025-05-12 16:33:18 +0300

    deleted *redundant* comment

--- app/src/main/java/com/example/summit/MainActivity.java
+++ app/src/main/java/com/example/summit/MainActivity.java
@@ -16,6 +16,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //! check with api call
     }
 }
